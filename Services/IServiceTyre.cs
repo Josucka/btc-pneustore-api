f04be444daeec0a5ce678cfd34462f5b535b662f
Josué Barros <frajolla27@gmail.com>
2021-09-22 15:52:25 -0300

    CreatescriadThe interface and service of the tire model.

---
+++ Services/IServiceTyre.cs
@@ -0,0 +1,16 @@
+﻿using ApiPneuStore.Models;
+using System.Collections.Generic;
+
+namespace ApiPneuStore.Services
+{
+    public interface IServiceTyre
+    {
+        List<Tyre> All();
+
+        Tyre Get(int id);
+
+        bool Create(Tyre tyre);
+
+        bool Update(Tyre tyre);
+    }
+}
---
+++ Services/TyreService.cs
@@ -0,0 +1,52 @@
+﻿using ApiPneuStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPneuStore.Services 
+{
+    public class TyreService : IServiceTyre
+    {
+        //construtor
+        public TyreService()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tyre> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(Tyre tyre)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+
+        public Tyre Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Tyre tyre)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
