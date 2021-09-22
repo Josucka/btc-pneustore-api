bf92c1fb449071b11ec56adf2dd7e21ece7f9c13
Josué Barros <frajolla27@gmail.com>
2021-09-21 23:58:57 -0300

    Commit first Test

---
+++ Services/IServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPneuStore.Services
+{
+    public class IServiceTest
+    {
+        //Teste Ok
+    }
+}
